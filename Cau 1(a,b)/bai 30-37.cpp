7c35f731d5133648c5f3da900090f6665f17e1e3
Quang Huy <luanquanghuy@gmail.com>
2017-12-08 11:52:09 +0700

    update bai 30-37

--- Cau 1(a,b)/bai 30-37.cpp
+++ Cau 1(a,b)/bai 30-37.cpp
@@ -40,6 +40,41 @@ int timSoDuongMax(int a[], int n){
         if(a[i]<0 && a[i]>soAmMax) soAmMax = a[i];
     return soAmMax;
 }
+// bai 34
+void tachSoNguyenTo(int a[], int &n, int b[], int &m){
+    int i, j, nUoc;
+    m = 0;
+    for(i=0; i<n; i++){
+        nUoc = 0;
+        for(j=1; j<=a[i]; j++)
+            if(a[i]%j==0) nUoc++;
+        if(nUoc==2){
+            b[m++] = a[i];
+            for(j=i; j<n-1; j++) a[j] = a[j+1];
+            n--;
+            i--;
+        }
+    }
+}
+// bai 35
+void tachSo(int a[], int n, int b[], int &n1, int c[], int n&2){
+    int i;
+    n1=0;
+    n2=0;
+    for(i=0; i<n; i++)
+        if(a[i]>0) b[n1++] = a[i];
+        else c[n2++] = a[i];
+}
+// bai 37
+void editSoNguyenTo(int a[], int n){
+    int i, j, nUoc;
+    for(i=0; i<n; i++){
+        nUoc = 0;
+        for(j=1; j<=a[i]; j++)
+            if(a[i]%j==0) nUoc++;
+        if(nUoc==2) a[i]=0;
+    }
+}
 int main(){
 	// Write code here
 	return 0;
